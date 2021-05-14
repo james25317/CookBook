731a739590d0be90fa5789e6fdcead8abbfc29ad
JamesHung <james49904230@gmail.com>
2021-05-14 16:05:59 +0800

    UI Structure: Re-organized profile page structure, including individual file of cell's nib

---
+++ CookBook/CookBook/View/Profile/ProfileCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileCollectionViewCell.swift
+//  CookBook
+//
+//  Created by James Hung on 2021/5/14.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
