65808197747ec47e8f754f388feb5128c3634bf0
JamesHung <james49904230@gmail.com>
2021-05-14 12:44:46 +0800

    UI Structure: Establish basic UI of each page (also segue linked), including Today, Feed and Profile in-half

---
+++ CookBook/CookBook/View/FeedTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  FeedTableViewCell.swift
+//  CookBook
+//
+//  Created by James Hung on 2021/5/13.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
