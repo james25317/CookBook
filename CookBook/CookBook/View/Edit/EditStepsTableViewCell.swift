c9aff288fcb156d50665038875fbfb0e1e4f047f
JamesHung <james49904230@gmail.com>
2021-05-15 14:11:46 +0800

    UI Structure: Establish Edit Page flow, edit preview page

---
+++ CookBook/CookBook/View/Edit/EditStepsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StepsTableViewCell.swift
+//  CookBook
+//
+//  Created by James Hung on 2021/5/15.
+//
+
+import UIKit
+
+class EditStepsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CookBook/CookBook/View/Read/ReadIngredientsTableViewCell.swift
+++ CookBook/CookBook/View/Read/ReadIngredientsTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class IngredientsTableViewCell: UITableViewCell {
+class ReadIngredientsTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
