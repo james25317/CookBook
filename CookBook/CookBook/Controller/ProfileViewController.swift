5169f0707311f76d35bef0d8e0f4b9ba2c652cfe
JamesHung <james49904230@gmail.com>
2021-05-18 20:40:05 +0800

    MainPage: Implemented buttons switch mechanism in Profile Page

--- CookBook/CookBook/Controller/ProfileViewController.swift
+++ CookBook/CookBook/Controller/ProfileViewController.swift
@@ -9,21 +9,56 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    private enum SortType: Int {
+
+        case recipes = 0
+
+        case favorites = 1
+
+        case challenges = 2
+    }
+
+    @IBOutlet weak var indicatorView: UIView!
+
+    @IBOutlet weak var indicatorCenterXConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var sortButtons: [UIButton]!
+
     override func viewDidLoad() {
+
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        sortButtons[0].isSelected = true
     }
-    
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onChangeSortType(_ sender: UIButton) {
+
+        for button in sortButtons {
+
+            button.isSelected = false
+        }
+
+        sender.isSelected = true
+
+        moveIndicatorView(reference: sender)
+
+        // guard let type = SortType(rawValue: sender.tag) else { return }
+
+        // updateContainer(type: type)
     }
-    */
 
+    private func moveIndicatorView(reference: UIView) {
+
+        indicatorCenterXConstraint.isActive = false
+
+        indicatorCenterXConstraint = indicatorView.centerXAnchor.constraint(equalTo: reference.centerXAnchor)
+
+        indicatorCenterXConstraint.isActive = true
+
+        UIView.animate(withDuration: 0.3, animations: { [weak self] in
+
+            self?.view.layoutIfNeeded()
+        })
+    }
 }
