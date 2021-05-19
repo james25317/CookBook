ef4a32b1435de105413090603043f2c9243a79ad
JamesHung <james49904230@gmail.com>
2021-05-19 20:51:16 +0800

    MainPage: Adjusted collectionView layout of ReadMode page

--- CookBook/CookBook/View/Read/ReadStepsCollectionViewCell.swift
+++ CookBook/CookBook/View/Read/ReadStepsCollectionViewCell.swift
@@ -10,8 +10,21 @@ import UIKit
 class ReadStepsCollectionViewCell: UICollectionViewCell {
 
     override func awakeFromNib() {
+
         super.awakeFromNib()
+
+        setupCellCardStyle()
+
         // Initialization code
+    }
+
+    private func setupCellCardStyle() {
+        
+        layer.cornerRadius = 8
+
+        layer.borderWidth = 1
+
+        layer.borderColor = UIColor.systemGray5.cgColor
     }
     
 }
