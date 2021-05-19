1da03665640a5cd3525f053a82cc1a546917857e
JamesHung <james49904230@gmail.com>
2021-05-19 12:09:13 +0800

    MainPage: Established UI Structure of Challenge Preview Page

---
+++ CookBook/CookBook/Controller/ChallengeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ChallengeViewController.swift
+//  CookBook
+//
+//  Created by James Hung on 2021/5/19.
+//
+
+import UIKit
+
+class ChallengeViewController: UIViewController {
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func close(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
