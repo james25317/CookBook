944c626d0a76e998887ceeedd63ad630f16a721b
JamesHung <james49904230@gmail.com>
2021-05-21 15:34:13 +0800

    ProfilePage: Established EditIngredients Page's tableView structure

--- CookBook/CookBook/Controller/EditIngredientsViewController.swift
+++ CookBook/CookBook/Controller/EditIngredientsViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class EditIngredientsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView! {
+
+        didSet {
+
+            tableView.delegate = self
+
+            tableView.dataSource = self
+        }
+    }
+
     override func viewDidLoad() {
 
         super.viewDidLoad()
+
+        setupTableView()
     }
 
     @IBAction func leave(_ sender: Any) {
@@ -27,4 +39,35 @@ class EditIngredientsViewController: UIViewController {
 
         dismiss(animated: true, completion: nil)
     }
+
+    private func setupTableView() {
+
+        tableView.registerCellWithNib(
+            identifier: String(describing: EditIngredientsTableViewCell.self),
+            bundle: nil
+        )
+    }
+}
+
+extension EditIngredientsViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 12
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: EditIngredientsTableViewCell.self), for: indexPath)
+
+        guard let ingredientCell = cell as? EditIngredientsTableViewCell else { return cell }
+
+        // 置換Firebase上的資料
+
+        return ingredientCell
+    }
+}
+
+extension EditIngredientsViewController: UITableViewDelegate {
+
 }
