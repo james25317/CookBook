da4e48a8f5860ad077aed0a6c761002d51fcdb73
JamesHung <james49904230@gmail.com>
2021-06-06 16:38:40 +0800

    AlertPopup: Install JGProgressHUD, setup environment.

--- CookBook/CookBook/AppDelegate.swift
+++ CookBook/CookBook/AppDelegate.swift
@@ -11,9 +11,15 @@ import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    static let shared = UIApplication.shared.delegate as! AppDelegate
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
--- CookBook/CookBook/ThirdPartyWrapper/KingFisherWrapper.swift
+++ CookBook/CookBook/ThirdPartyWrapper/KingFisherWrapper.swift
@@ -10,9 +10,12 @@ import Kingfisher
 
 extension UIImageView {
 
-  func loadImage(_ urlString: String?, placeHolder: UIImage? = nil) {
-    guard urlString != nil else { return }
-    let url = URL(string: urlString!)
-    self.kf.setImage(with: url, placeholder: placeHolder)
-  }
+    func loadImage(_ urlString: String?, placeHolder: UIImage? = nil) {
+
+        guard urlString != nil else { return }
+
+        let url = URL(string: urlString!)
+        
+        self.kf.setImage(with: url, placeholder: placeHolder)
+    }
 }
